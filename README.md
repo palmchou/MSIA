22647c45574ea23b2ea793a6ec9acf7b92437ab6
Palm <palmchou@live.com>
2014-11-06 11:58:17 +0800

    Configure routes. 頁面路由配置

--- README.md
+++ README.md
@@ -11,12 +11,13 @@ run `rake db:migrate:reset && rake db:setup` to initialize the database.
 
 ### Things should know before you contribute
 1. You must commit with a **commit message**. Both English or Chinese you can use as the language in your commit message.
+but you can't use Chinese in comments in codes.
 
 2. Remember to **format** your code before committing.
 
 3. You can open an [issue](https://github.com/palmchou/MSIA/issues) if you have any questions of any codes that written by others or some big bugs you found in this project.
 
 ### 須知
-1. 每次commit必須要含有**commit message**，中文英文都可以。
+1. 每次commit必須要含有**commit message**，中文英文都可以。但代码的注释不要使用中文。
 2. 記得**format**你的代碼，在你commit之前。
 3. 如果你對其他人貢獻的代碼有疑問或質疑，或發現重大bug，你可以創建一個[issue](https://github.com/palmchou/MSIA/issues)，供來討論。
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,37 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  # Routes for about
+  scope '/about' do
+    get 'info', to: 'about#info', as: 'about_info'
+    get 'structure', to: 'about#structure', as: 'about_structure'
+    get 'purpose', to: 'about#purpose', as: 'about_purpose'
+  end
+  # Routes for news
+  scope '/news' do
+    get 'notice', to: 'news#notice', as: 'news_notice'
+    get 'msia', to: 'news#msia', as: 'news_msia'
+    get 'industry', to: 'news#industry', as: 'news_industry'
+  end
+
+  # Routes for recommend software products
+  resources :recommendations
+
+  # Routes for member services
+  resources :members
+  get 'members/join-us', to: 'members#getJoin', as: 'members_join'
+
+  # Routes for talents service
+  resources :talents
+
+  # Routes for friendly links
+  resources :links
+
+  # Routes for contact us page
+  get 'contact', to: 'home#contact', as: 'contact_us'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
