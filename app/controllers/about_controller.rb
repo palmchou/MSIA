c1da704a757eb5502113d868ce3cf0589904e629
Palm <palmchou@live.com>
2014-11-06 11:59:07 +0800

    Create and initial controllers

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,10 @@
+class AboutController < ApplicationController
+  def info
+  end
+
+  def structure
+  end
+
+  def purpose
+  end
+end
---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,10 @@
+class NewsController < ApplicationController
+  def notice
+  end
+
+  def msia
+  end
+
+  def industry
+  end
+end
