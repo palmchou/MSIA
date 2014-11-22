76e27f2b998c7f52ffbcf9602d941baf67c707ca
Palm <palmchou@live.com>
2014-11-22 15:48:39 +0800

    Added and deployed devise gem for user module.

--- Gemfile
+++ Gemfile
@@ -56,9 +56,6 @@ group :development, :test do
   # Call 'debugger' anywhere in the code to stop execution and get a debugger console
   #gem 'byebug'
 
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~>1.0.4'
-
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,16 +13,13 @@ Rails.application.routes.draw do
     get 'notice', to: 'news#notice', as: 'news_notice'
     get 'msia', to: 'news#msia', as: 'news_msia'
     get 'industry', to: 'news#industry', as: 'news_industry'
-    get '3', to: 'news#news3', as: 'news_3'
-    get '4', to: 'news#news4', as: 'news_4'
-    get '5', to: 'news#news5', as: 'news_5'
-    get '6', to: 'news#news6', as: 'news_6'
-    get '7', to: 'news#news7', as: 'news_7'
   end
   resources :news
 
   # Routes for recommend software products
   # resources :recommendations
+
+  devise_for :users
 
   # Routes for member services
   get 'members/login', to: 'members#login', as: 'members_login'
@@ -39,59 +36,4 @@ Rails.application.routes.draw do
   # Routes for contact us page
   get 'contact', to: 'home#contact', as: 'contact_us'
 
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
